ec5b9c97a3bd50d655e7ce0ff56595d60d9df100
damnvaz <45245879+damnvaz@users.noreply.github.com>
2020-10-20 17:06:29 -0300

    Update ButtonManager.cs

--- jooj - the journey/Assets/ButtonManager.cs
+++ jooj - the journey/Assets/ButtonManager.cs
@@ -5,11 +5,48 @@ using UnityEngine.SceneManagement;
 public class ButtonManager : MonoBehaviour {
 
 
-	public void PlayGameBtn(string newGameLevel) {
+    public void PlayGameBtn(string LoadingScene1)
+    {
 
-		SceneManager.LoadScene(newGameLevel);		
+        SceneManager.LoadScene(LoadingScene1);
 
-	}
+    }
+
+
+    void Update()
+    {
+        OnGUI();
+    }
+
+    void OnGUI()
+    {
+
+        Scene scene = SceneManager.GetActiveScene();
+
+        if (Event.current.Equals(Event.KeyboardEvent("enter")) && scene.name.Equals("LoadingScene1"))
+        {
+            StartCoroutine(LoadAsynchronously(2));
+        }
+
+        if (Event.current.Equals(Event.KeyboardEvent("escape")) && scene.name.Equals("LoadBackMenu"))
+        {
+            Application.LoadLevel(3);
+
+            StartCoroutine(LoadAsynchronously(0));
+        }
+
+    }
+
+    IEnumerator LoadAsynchronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            yield return null;
+        }
+    }
+
 
     public void LoadGameBtn(string MainMenuScreen) {
 
